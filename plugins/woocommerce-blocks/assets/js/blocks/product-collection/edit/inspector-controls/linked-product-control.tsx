4ca6696f12a955f753fd20927077813c48de78db
Manish Menaria <the.manish.menaria@gmail.com>
2024-10-25 14:56:39 +0530

    Product Collection: Add "order" reference to Upsells and Cross-sells (#52258)
    * Add order reference to Upsells and Cross-sells
    
    - Added 'order' to usesReference in cross-sells and upsells
    - Updated LinkedProductControl for order context
    - Enhanced ProductCollection to handle order references
    
    * Add changefile(s) from automation for the following project(s): woocommerce-blocks, woocommerce
    
    * Add PHP code for Cross-sells collection
    
    * Fix formatting
    
    * Add E2E tests
    
    1. In the Cart Template, it checks that "From products in the cart" is
       selected by default for the "My Custom Collection - Cart Context".
    
    2. In the Order Confirmation Template, it verifies that "From products
       in the order" is chosen by default for the "My Custom Collection -
       Order Context".
    
    * Add 'block' scope to cross-sells collection
    
    * Extract and use reference type constants
    
    This commit extracts the REFERENCE_TYPE_CART and REFERENCE_TYPE_ORDER
    constants to the top of the file and updates their usage throughout
    the code, including in the getFromCurrentProductRadioLabel function.
    
    * Align variable naming for product references in Product Collection
    
    This commit updates the ProductCollection class to ensure consistency in
    variable naming for product references across different collection types,
    particularly aligning with the naming convention used in Upsells.
    
    * Add "hadOrderReference" to conditional label display
    
    Key changes:
    - Removed `isUsesReferenceIncludesCart` parameter from `getFromCurrentProductRadioLabel`
    - Introduced `hasCartReference` and `hasOrderReference` parameters for consistency
    - Updated conditional logic to check for both cart and order references
    - Adjusted the function call in `LinkedProductControl` to pass both new parameters
    
    * Fallback to 0 for orderId in get_product_ids_from_order calls
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-controls/linked-product-control.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-controls/linked-product-control.tsx
@@ -32,6 +32,10 @@ import type {
 	ProductCollectionSetAttributes,
 } from '../../types';
 
+const REFERENCE_TYPE_PRODUCT = 'product';
+const REFERENCE_TYPE_CART = 'cart';
+const REFERENCE_TYPE_ORDER = 'order';
+
 const ProductButton: React.FC< {
 	isOpen: boolean;
 	onToggle: () => void;
@@ -127,10 +131,15 @@ const enum PRODUCT_REFERENCE_TYPE {
 
 const getFromCurrentProductRadioLabel = (
 	currentLocation: string,
-	isUsesReferenceIncludesCart: boolean
+	hasCartReference: boolean,
+	hasOrderReference: boolean
 ): string => {
-	if ( currentLocation === 'cart' && isUsesReferenceIncludesCart ) {
+	if ( currentLocation === REFERENCE_TYPE_CART && hasCartReference ) {
 		return __( 'From products in the cart', 'woocommerce' );
+	}
+
+	if ( currentLocation === REFERENCE_TYPE_ORDER && hasOrderReference ) {
+		return __( 'From products in the order', 'woocommerce' );
 	}
 
 	return __( 'From the current product', 'woocommerce' );
@@ -147,21 +156,24 @@ const LinkedProductControl = ( {
 	location: WooCommerceBlockLocation;
 	usesReference: string[] | undefined;
 } ) => {
-	const REFERENCE_TYPE_PRODUCT = 'product';
 	const isProductLocation = location.type === REFERENCE_TYPE_PRODUCT;
 	const hasProductReference = !! usesReference?.includes(
 		REFERENCE_TYPE_PRODUCT
 	);
-	const REFERENCE_TYPE_CART = 'cart';
 	const isCartLocation = location.type === REFERENCE_TYPE_CART;
 	const hasCartReference = !! usesReference?.includes( REFERENCE_TYPE_CART );
+
+	const isOrderLocation = location.type === REFERENCE_TYPE_ORDER;
+	const hasOrderReference =
+		!! usesReference?.includes( REFERENCE_TYPE_ORDER );
+
 	const { productReference } = query;
 
 	const { product, isLoading } = useGetProduct( productReference );
 	const [ isDropdownOpen, setIsDropdownOpen ] = useState< boolean >( false );
 	const [ radioControlState, setRadioControlState ] =
 		useState< PRODUCT_REFERENCE_TYPE >(
-			( isProductLocation || isCartLocation ) &&
+			( isProductLocation || isCartLocation || isOrderLocation ) &&
 				isEmpty( productReference )
 				? PRODUCT_REFERENCE_TYPE.CURRENT_PRODUCT
 				: PRODUCT_REFERENCE_TYPE.SPECIFIC_PRODUCT
@@ -170,7 +182,8 @@ const LinkedProductControl = ( {
 
 	const showRadioControl =
 		( isProductLocation && hasProductReference ) ||
-		( isCartLocation && hasCartReference );
+		( isCartLocation && hasCartReference ) ||
+		( isOrderLocation && hasOrderReference );
 	const showSpecificProductSelector = showRadioControl
 		? radioControlState === PRODUCT_REFERENCE_TYPE.SPECIFIC_PRODUCT
 		: ! isEmpty( productReference );
@@ -179,9 +192,8 @@ const LinkedProductControl = ( {
 		( showRadioControl || showSpecificProductSelector ) &&
 		/**
 		 * Linked control is only useful for collection which uses product, cart or order reference.
-		 * TODO Add handling for Order reference
 		 */
-		( hasProductReference || hasCartReference );
+		( hasProductReference || hasCartReference || hasOrderReference );
 	if ( ! showLinkedProductControl ) return null;
 
 	const radioControlHelp =
@@ -215,7 +227,8 @@ const LinkedProductControl = ( {
 
 	const fromCurrentProductRadioLabel = getFromCurrentProductRadioLabel(
 		location.type,
-		hasCartReference
+		hasCartReference,
+		hasOrderReference
 	);
 
 	return (
