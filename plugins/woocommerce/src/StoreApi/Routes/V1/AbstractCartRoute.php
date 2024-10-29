8fa2797a6ff1a14c5118146e39f08783d4e8f4ec
Alex Florisca <alex.florisca@automattic.com>
2024-10-29 14:54:26 +0000

    Check for valid session object before calling get_customer_id in Cart Route (#52410)
    * Check for valid session object before calling get_customer_id in Cart Route
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Harden get_cart_token function to make sure the cart is loaded before we access the session
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/src/StoreApi/Routes/V1/AbstractCartRoute.php
+++ plugins/woocommerce/src/StoreApi/Routes/V1/AbstractCartRoute.php
@@ -188,6 +188,13 @@ abstract class AbstractCartRoute extends AbstractRoute {
 	 * @return string
 	 */
 	protected function get_cart_token() {
+		// Ensure cart is loaded.
+		$this->cart_controller->load_cart();
+
+		if ( ! wc()->session ) {
+			return null;
+		}
+
 		return JsonWebToken::create(
 			[
 				'user_id' => wc()->session->get_customer_id(),
