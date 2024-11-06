a39445d6915d525e8b71126da0dc2c9ded8ec4b9
Alba Rincón <albarin@users.noreply.github.com>
2024-11-06 01:08:47 +0100

    Show the payment method as text if there's no `brand` or `last4` digits are present (#52578)
    * Show the payment method as text if there's no brand or last4 digits
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/src/Internal/ReceiptRendering/Templates/order-receipt.php
+++ plugins/woocommerce/src/Internal/ReceiptRendering/Templates/order-receipt.php
@@ -25,13 +25,13 @@
 	<?php if ( isset( $data['payment_method'] ) ) { ?>
 		<h3 id="payment_method_section_title"><?php echo strtoupper( $data['texts']['payment_method_section_title'] ); ?></h3>
 		<p>
-			<?php if ( $data['payment_info'] ) { ?>
+			<?php if ( $data['show_payment_method_title'] ) { ?>
+				<span><?php echo $data['payment_method']; ?></span>
+			<?php } else { ?>
 				<span class="card-icon"></span>
 				<?php if ( $data['payment_info']['card_last4'] ) { ?>
 					- <?php echo $data['payment_info']['card_last4']; ?>
 				<?php } ?>
-			<?php } else { ?>
-				<span><?php echo $data['payment_method']; ?></span>
 			<?php } ?>
 		</p>
 	<?php } ?>
