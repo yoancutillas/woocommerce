97996a9f0a33333572096860cf60ac64d118fe9a
Joshua T Flowers <joshuatf@gmail.com>
2024-11-07 17:10:30 -0500

    Add frontend metric tests (#52347)
    * Add frontend metric tests
    
    * Add changelog entry
    
    * Fix linting issues
    
    ---------
    
    Co-authored-by: Vladimir Reznichenko <kalessil@gmail.com>

--- .github/workflows/scripts/run-metrics.sh
+++ .github/workflows/scripts/run-metrics.sh
@@ -44,6 +44,7 @@ if [ "$GITHUB_EVENT_NAME" == "push" ] || [ "$GITHUB_EVENT_NAME" == "pull_request
 		title "##[group]Benchmarking head"
 		RESULTS_ID="editor_${GITHUB_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics editor
 		RESULTS_ID="product-editor_${GITHUB_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics product-editor
+		RESULTS_ID="frontend_${GITHUB_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics frontend
 		echo '##[endgroup]'
 	fi
 
@@ -66,6 +67,7 @@ if [ "$GITHUB_EVENT_NAME" == "push" ] || [ "$GITHUB_EVENT_NAME" == "pull_request
 		git restore --source $GITHUB_SHA $(realpath $(dirname -- ${BASH_SOURCE[0]})/../../../plugins/woocommerce/tests)
 		RESULTS_ID="editor_${BASE_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics editor
 		RESULTS_ID="product-editor_${BASE_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics product-editor
+		RESULTS_ID="frontend_${BASE_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics frontend
 		echo '##[endgroup]'
 
 		# title "##[group]Restoring codebase state back to head"
