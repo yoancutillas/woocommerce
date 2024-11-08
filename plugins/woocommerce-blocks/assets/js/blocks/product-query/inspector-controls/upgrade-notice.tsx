ef50c56bc859f9adc7d13c54ef8ad668261528fd
Fernando Marichal <fernando.marichal@automattic.com>
2024-11-08 13:54:32 -0300

    Product Collection Telemetry: Convert Product to Product Collection and revert to Products (#52651)
    * Record Products to Product Collection
    
    * Add changelog
    
    * Update plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-controls/upgrade-notice.tsx
    
    Co-authored-by: Karol Manijak <20098064+kmanijak@users.noreply.github.com>
    
    * Fix lint
    
    * Rename event name
    
    ---------
    
    Co-authored-by: Karol Manijak <20098064+kmanijak@users.noreply.github.com>

--- plugins/woocommerce-blocks/assets/js/blocks/product-query/inspector-controls/upgrade-notice.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-query/inspector-controls/upgrade-notice.tsx
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { Notice, Button } from '@wordpress/components';
 import { createInterpolateElement } from '@wordpress/element';
+import { recordEvent } from '@woocommerce/tracks';
 
 export const UpgradeNotice = ( props: { upgradeBlock: () => void } ) => {
 	const notice = createInterpolateElement(
@@ -22,6 +23,12 @@ export const UpgradeNotice = ( props: { upgradeBlock: () => void } ) => {
 
 	const handleClick = () => {
 		props.upgradeBlock();
+		recordEvent(
+			'blocks_product_collection_migration_between_products_beta',
+			{
+				transform_to: 'product_collection',
+			}
+		);
 	};
 
 	return (
