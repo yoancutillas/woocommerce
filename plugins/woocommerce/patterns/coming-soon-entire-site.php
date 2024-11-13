a518c4951734838a1fb43a5688478dfd8406214e
Ilyas Foo <foo.ilyas@gmail.com>
2024-11-13 16:09:32 +0800

    Add deprecated note to existing coming soon templates and apply fix from previous PR (#52762)
    * Add deprecated note and apply fix from previous PR
    
    * Changelog
    
    * Lint

--- plugins/woocommerce/patterns/coming-soon-entire-site.php
+++ plugins/woocommerce/patterns/coming-soon-entire-site.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * Note: This pattern is deprecated, it will be removed once newsletter feature flag is deployed.
+ * If you are updating this pattern, please also update page-coming-soon-default.php.
+ */
+
+/**
  * Title: Coming Soon Entire Site
  * Slug: woocommerce/coming-soon-entire-site
  * Categories: WooCommerce
